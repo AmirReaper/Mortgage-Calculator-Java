a1c238f6e969b30cfd62cca56c0cf4b4937bfbd0
AmirReaper <amirrep98@gmail.com>
2025-09-05 01:02:19 +0330

    refactor: UPDATE MortgageReport for formatted output of payments and schedules

--- Mortgage Calculator/src/com/example/mortgage/MortgageReport.java
+++ Mortgage Calculator/src/com/example/mortgage/MortgageReport.java
@@ -3,32 +3,17 @@ package com.example;
 import java.text.NumberFormat;
 
 /**
- * MortgageReport is responsible for displaying the results of calculations
- * to the user. It separates business logic (calculations) from presentation.
+ * Abstract report that knows *what* to show but not *how* to output.
+ * Subclasses decide the output channel (console, file, etc.).
  */
-public class MortgageReport {
-    private final MortgageCalculator calculator;
+public abstract class MortgageReport {
+    protected final Mortgage mortgage;
+    protected final NumberFormat currency = NumberFormat.getCurrencyInstance();
 
-    public MortgageReport(MortgageCalculator calculator) {
-        this.calculator = calculator;
+    protected MortgageReport(Mortgage mortgage) {
+        this.mortgage = mortgage;
     }
 
-    public void printMortgage() {
-        double mortgage = calculator.calculateMortgage();
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-
-        System.out.println("\nMortgage");
-        System.out.println("=======================================");
-        System.out.println("Monthly Payment: " + mortgageFormatted);
-    }
-
-    public void printPaymentSchedule() {
-        System.out.println("\nPayment Schedule");
-        System.out.println("=======================================");
-
-        for (int month = 1; month <= calculator.getYears() * 12; month++) {
-            double balance = calculator.calculateBalance(month);
-            System.out.printf("Month %3d: %15s%n", month, NumberFormat.getCurrencyInstance().format(balance));
-        }
-    }
+    public abstract void printMortgage();
+    public abstract void printPaymentSchedule();
 }
