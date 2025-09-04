dc3d801cbcbae4d4824cdc3309bf862ef794e924
AmirReaper <amirrep98@gmail.com>
2025-09-05 00:56:39 +0330

    feat: implement AdjustableRateMortgage with initial vs adjusted calculation

---
+++ Mortgage Calculator/src/com/example/mortgage/AdjustableRateMortgage.java
@@ -0,0 +1,33 @@
+package com.example;
+
+/**
+ * Adjustable-Rate Mortgage (ARM) example.
+ * For simplicity, monthly payment is computed using the *current* annual rate.
+ * You can call adjustAnnualRate(...) and then re-run calculations.
+ */
+public class AdjustableRateMortgage extends Mortgage implements Adjustable {
+
+    public AdjustableRateMortgage(int principal, float annualInterestPercent, byte years) {
+        super(principal, annualInterestPercent, years);
+    }
+
+    @Override
+    public void adjustAnnualRate(float newAnnualPercent) {
+        // Uses protected setter from base class (access via 'super' API)
+        setAnnualInterestPercent(newAnnualPercent);
+    }
+
+    @Override
+    public double calculateMonthlyPayment() {
+        return MortgageMath.monthlyPayment(
+                getPrincipal(), monthlyRate(), totalPayments()
+        );
+    }
+
+    @Override
+    public double calculateBalance(int numberOfPaymentsMade) {
+        return MortgageMath.remainingBalance(
+                getPrincipal(), monthlyRate(), totalPayments(), numberOfPaymentsMade
+        );
+    }
+}
