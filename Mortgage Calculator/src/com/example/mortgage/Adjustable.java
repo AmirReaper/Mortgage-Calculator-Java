c73924d1175a933147c0b3f8b2827faf111e9709
AmirReaper <amirrep98@gmail.com>
2025-09-05 00:58:01 +0330

    feat: implement Interface for adjustable-rate behavior (simulates multiple inheritance).

---
+++ Mortgage Calculator/src/com/example/mortgage/Adjustable.java
@@ -0,0 +1,8 @@
+package com.example;
+
+/**
+ * Interface for adjustable-rate behavior (simulates multiple inheritance).
+ */
+public interface Adjustable {
+    void adjustAnnualRate(float newAnnualPercent);
+}
